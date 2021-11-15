8b82f54211686fbb7159e93cba76da8c3114d719
Chucky Master <user.email>
2021-11-15 21:49:47 +0100

    probleme image et card qui se duplique

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 "use strict";
 
 /** VARIABLES */
-
+let btnAdd;
+let form;
 /**    ARRAY OBJECT  */
-let listAlbum = {
-  img: "imgcold.jpg",
-  nomArtiste: "Gideon",
-  titre: "Cold",
-  annee: 1998,
-  label: "Arise",
-  duree: 45,
-};
+let listAlbum = new Array();
+
+// let alb1 = {
+//   img: "/img/cold.jpg",
+//   nomArtiste: "Gideon",
+//   titre: "Cold",
+//   annee: 1998,
+//   label: "Arise",
+//   duree: 45 + " minutes",
+// };
 
 /**FUNCTION */
-
+//afficher un album
 function displayAlbum(album) {
   let sectionAlbum = document.querySelector("#album");
   let div = document.createElement("div");
@@ -21,7 +24,7 @@ function displayAlbum(album) {
   div.classList.add("card");
   sectionAlbum.appendChild(div);
   let img = document.createElement("img");
-  img.setAttribute("src", "");
+  img.setAttribute("src", `${album.img}`);
   div.appendChild(img);
   let divD = document.createElement("div");
   divD.classList.add("description");
@@ -29,27 +32,59 @@ function displayAlbum(album) {
   div.appendChild(divD);
   let ul = document.createElement("ul");
   divD.appendChild(h3);
-  divD.appendChild(ul);
 
   h3.innerHTML += ` ${album.nomArtiste}`;
   for (const property in album) {
-    console.log(album[property]);
+    //verifier la propriété:
+    // console.log(album[property]);
     let li = document.createElement("li");
     ul.appendChild(li);
 
+    //Affiche la propriété et le valeur
     li.innerHTML += `${property} : ${album[property]}`;
-    // li.innerHTML += `${album.annee}`;
-
-    // li.innerHTML += `${album.label}`;
-    // li.innerHTML += `${album.duree}`;
   }
+
+  form.reset();
+  divD.appendChild(ul);
 }
+
+//Reccupérer les input
+
+function catchValue() {
+  let album = new Object();
+  album.img = document.querySelector("#image").files[0].name;
+  album.nomArtiste = document.querySelector("#artiste").value;
+  album.titre = document.querySelector("#titre").value;
+  album.annee = document.querySelector("#annee").value;
+  album.label = document.querySelector("#label").value;
+  album.duree = document.querySelector("#duree").value;
+
+  console.log(album.img);
+  listAlbum.push(album);
+  displayArrayAlbum(listAlbum);
+}
+
+//afficher le tabeal listAlbum
+function displayArrayAlbum() {
+  listAlbum.forEach((elem) => {
+    displayAlbum(elem);
+  });
+}
+
+//Ajouter event de la fonction catchValue au button "ajouter"
 
 /** MAIN CODE */
 
 document.addEventListener("DOMContentLoaded", function () {
   /** SELECTORS */
+  form = document.querySelector("form");
+  btnAdd = document.querySelector("#ajouter");
   /**EVENT */
 
-  displayAlbum(listAlbum);
+  btnAdd.addEventListener("click", catchValue);
+  //Empeche le raffraichissement par defaut de la page au click sur le bouton submit
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    return;
+  });
 });
